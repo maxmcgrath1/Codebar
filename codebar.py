edc75a30d50c47211b36bedef828d8e4db0c4764
maxmcgrath1 <tuc06096@temple.edu>
2022-02-02 17:03:09 -0500

    added greeting

--- codebar.py
+++ codebar.py
@@ -1,9 +1,23 @@
 class Member:
     def __init__(self, full_name,):
         self.full_name = full_name
+    
+    def greet(self):
+        print(f"Hi, my name is {self.full_name}")
 
 class Student(Member):
     def __init__(self, full_name, reason):
         super().__init__(full_name)
         self.reason = reason
+
+class Instructor(Member):
+    def __init__(self, full_name, bio, skills):
+        super().__init__(full_name)
+        self.bio = bio
+        self.skills = skills
+
+
+
+
+
 

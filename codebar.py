6d410b191475e960ea8be23d15a44b71fe9ab4e1
maxmcgrath1 <tuc06096@temple.edu>
2022-02-02 16:57:43 -0500

    Created Student class

--- codebar.py
+++ codebar.py
@@ -2,4 +2,8 @@ class Member:
     def __init__(self, full_name,):
         self.full_name = full_name
 
-``
+class Student(Member):
+    def __init__(self, full_name, reason):
+        super().__init__(full_name)
+        self.reason = reason
+
